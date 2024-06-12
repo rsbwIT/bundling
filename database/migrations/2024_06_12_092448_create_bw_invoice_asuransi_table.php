cb196326625be0f6818505eb3cf4ebeb4513e16d
unknown <feri.18421026@student.ubl.ac.id>
2024-06-12 09:33:13 +0700

    update menu cob piutang

--- database/migrations/2024_06_12_092448_create_bw_invoice_asuransi_table.php
+++ database/migrations/2024_06_12_092448_create_bw_invoice_asuransi_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('bw_invoice_asuransi', function (Blueprint $table) {
             $table->string('nomor_tagihan', 50)->primary();
-            $table->string('kode_asuransi', 100);
-            $table->string('nama_asuransi', 100);
-            $table->string('alamat_asuransi', 150);
+            $table->string('kode_asuransi', 100)->nullable();
+            $table->string('nama_asuransi', 100)->nullable();
+            $table->string('alamat_asuransi', 150)->nullable();
         });
     }
 
