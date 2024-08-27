b4739ff66851b12e1c1eb42021156d845ba776e8
unknown <feri.18421026@student.ubl.ac.id>
2024-08-27 09:37:49 +0700

    Anjungan Mandiri

--- resources/views/layout/layoutDashboard.blade.php
+++ resources/views/layout/layoutDashboard.blade.php
@@ -558,6 +558,14 @@
                                     </a>
                                 </li>
                             </ul>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="{{ url('info-kamar2') }}" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Info Kamar 2</p>
+                                    </a>
+                                </li>
+                            </ul>
 
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
