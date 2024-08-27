1ba9e6b5d3eb7bb24b5cf9abeac9d4340c266fc3
unknown <feri.18421026@student.ubl.ac.id>
2024-08-27 09:36:32 +0700

    Anjungan Mandiri

--- resources/views/livewire/info-kamar/info-kamar2.blade.php
+++ resources/views/livewire/info-kamar/info-kamar2.blade.php
@@ -1,26 +1,32 @@
 <div>
-    <div class="card">
-        <div class="card-body p-0">
-            <table class="table table-striped">
-                <thead>
-                    <tr class="h4">
-                        <th style="width: 10px">#</th>
-                        <th width="60%">Kelas</th>
-                        <th width="20" class="text-center">Progress</th>
-                        <th width="20" class="text-center">Label</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($getKamar as $key => $item)
-                        <tr class="h5">
-                            <td>{{ $key + 1 }}</td>
-                            <td>{{ $item->kelas }}</td>
-                            <td class="text-center">{{ $item->total_status_1 }}</td>
-                            <td class="text-center">{{ $item->total_status_0 }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+    <div class="col-md-12">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-body p-0">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr class="h4">
+                                    <th style="width: 10px">#</th>
+                                    <th width="60%">Kelas</th>
+                                    <th width="20" class="text-center">Progress</th>
+                                    <th width="20" class="text-center">Label</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($getKamar as $key => $item)
+                                    <tr class="h5">
+                                        <td>{{ $key + 1 }}</td>
+                                        <td>{{ $item->kelas }}</td>
+                                        <td class="text-center">{{ $item->total_status_1 }}</td>
+                                        <td class="text-center">{{ $item->total_status_0 }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
