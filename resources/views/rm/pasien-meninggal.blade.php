f456a6814425da8c5c2a887c11e8e9283f8bb729
unknown <feri.18421026@student.ubl.ac.id>
2025-01-20 15:52:38 +0700

    config lis

--- resources/views/rm/pasien-meninggal.blade.php
+++ resources/views/rm/pasien-meninggal.blade.php
@@ -4,14 +4,6 @@
     @livewireStyles
 @endpush
 @section('konten')
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-body">
-                </div>
-            </div>
-        </div>
-    </div>
     <div class="card">
         <div class="card-body">
 
