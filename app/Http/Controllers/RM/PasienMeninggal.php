c00318862e44da90950f3bcbd8c141ede6535703
unknown <feri.18421026@student.ubl.ac.id>
2025-01-20 15:54:31 +0700

    config lis

--- app/Http/Controllers/RM/PasienMeninggal.php
+++ app/Http/Controllers/RM/PasienMeninggal.php
@@ -20,37 +20,36 @@ class PasienMeninggal extends Controller
         // $kdPenjamin = ($request->input('kdPenjamin') == null) ? "" : explode(',', $request->input('kdPenjamin'));
         // $penjab = $this->cacheService->getPenjab();
         $data = DB::table('pasien_mati')
-    ->join('pasien', 'pasien_mati.no_rkm_medis', '=', 'pasien.no_rkm_medis')
-    ->join('dokter', 'pasien_mati.kd_dokter', '=', 'dokter.kd_dokter')
-    ->select(
-        'pasien_mati.tanggal',
-        'pasien_mati.jam',
-        'pasien_mati.no_rkm_medis',
-        'pasien.nm_pasien',
-        'pasien.jk',
-        'pasien.tmp_lahir',
-        'pasien.tgl_lahir',
-        'pasien.gol_darah',
-        'pasien.stts_nikah',
-        'pasien.agama',
-        'pasien_mati.keterangan',
-        'pasien_mati.temp_meninggal',
-        'pasien_mati.icd1',
-        'pasien_mati.icd2',
-        'pasien_mati.icd3',
-        'pasien_mati.icd4',
-        'dokter.nm_dokter'
-    )
-    ->whereBetween('pasien_mati.tanggal', [$request->tgl1, $request->tgl2])
-    ->orderBy('pasien_mati.tanggal', 'desc');
-            // ->get();
-            // dd($data);
-            $results = $data->paginate(1000);
+            ->join('pasien', 'pasien_mati.no_rkm_medis', '=', 'pasien.no_rkm_medis')
+            ->join('dokter', 'pasien_mati.kd_dokter', '=', 'dokter.kd_dokter')
+            ->select(
+                'pasien_mati.tanggal',
+                'pasien_mati.jam',
+                'pasien_mati.no_rkm_medis',
+                'pasien.nm_pasien',
+                'pasien.jk',
+                'pasien.tmp_lahir',
+                'pasien.tgl_lahir',
+                'pasien.gol_darah',
+                'pasien.stts_nikah',
+                'pasien.agama',
+                'pasien_mati.keterangan',
+                'pasien_mati.temp_meninggal',
+                'pasien_mati.icd1',
+                'pasien_mati.icd2',
+                'pasien_mati.icd3',
+                'pasien_mati.icd4',
+                'dokter.nm_dokter'
+            )
+            ->whereBetween('pasien_mati.tanggal', [$request->tgl1, $request->tgl2])
+            ->orderBy('pasien_mati.tanggal', 'desc');
+        // ->get();
+        // dd($data);
+        $results = $data->paginate(1000);
 
-            return  view("rm.pasien-meninggal", [
-                'results' => $results,
-                // 'penjab' => $penjab,
-            ]);
-
+        return  view("rm.pasien-meninggal", [
+            'results' => $results,
+            // 'penjab' => $penjab,
+        ]);
     }
 }
