f2d4bf069422ff46141a01c87e4ae458bfc5b9df
REZAALAMSYAHRAPI <email@example.com>
2025-03-11 08:49:36 +0700

    casemixrawatinapresume3

--- resources/views/bpjs/component/print/resume-pasien.blade.php
+++ resources/views/bpjs/component/print/resume-pasien.blade.php
@@ -1,5 +1,5 @@
 @if ($getResume && $statusLanjut)
-    @if ($statusLanjut->kd_poli === 'U0085' || $statusLanjut->kd_poli === 'U0085')
+    @if ($statusLanjut->kd_poli === 'U0061' || $statusLanjut->kd_poli === 'FIS')
         <div class="card-body">
             <div class="card p-4 d-flex justify-content-center align-items-center">
                 <table width="700px">
