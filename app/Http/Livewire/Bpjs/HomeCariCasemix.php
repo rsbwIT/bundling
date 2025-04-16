831db70549cb0a3ed7df749b618cb08ea54cb4a8
REZAALAMSYAHRAPI <email@example.com>
2025-04-16 11:49:12 +0700

    cepatkan1

--- app/Http/Livewire/Bpjs/HomeCariCasemix.php
+++ app/Http/Livewire/Bpjs/HomeCariCasemix.php
@@ -59,67 +59,45 @@ class HomeCariCasemix extends Component
     //         ->get();
 
     public function getPasien()
-{
-
-    ini_set('max_execution_time', 10);
-    if ($this->cariNorawat) {
-        $this->getPasien = DB::table('reg_periksa')
-            ->select(
-                'reg_periksa.no_rawat',
-                'reg_periksa.no_rkm_medis',
-                'reg_periksa.tgl_registrasi',
-                'pasien.nm_pasien',
-                'pasien.jk',
-                DB::raw("COALESCE(bridging_ranap.no_sep, bridging_ralan.no_sep, '-') AS no_sep"),
-                DB::raw("
-                    CASE
-                        WHEN reg_periksa.status_lanjut = 'Ranap' THEN '1'
-                        WHEN reg_periksa.status_lanjut = 'Ralan' THEN '2'
-                        ELSE '-'
-                    END AS jnspelayanan
-                "),
-                'reg_periksa.status_lanjut'
-            )
-            ->join('pasien', 'reg_periksa.no_rkm_medis', '=', 'pasien.no_rkm_medis')
-            ->leftJoin(DB::raw("
-                (SELECT bridging_sep.no_rawat, bridging_sep.no_sep
-                 FROM bridging_sep
-                 WHERE bridging_sep.jnspelayanan = '1'
-                 AND bridging_sep.no_sep > 2
-                 AND bridging_sep.no_sep = (
-                     SELECT MIN(bridging_sep_inner.no_sep)
-                     FROM bridging_sep bridging_sep_inner
-                     WHERE bridging_sep_inner.jnspelayanan = '1'
-                     AND bridging_sep_inner.no_rawat = bridging_sep.no_rawat
-                     AND bridging_sep_inner.no_sep > 2
-                 )
-                ) AS bridging_ranap
-            "), 'bridging_ranap.no_rawat', '=', 'reg_periksa.no_rawat')
-            ->leftJoin(DB::raw("
-                (SELECT bridging_sep.no_rawat, bridging_sep.no_sep
-                 FROM bridging_sep
-                 WHERE bridging_sep.jnspelayanan = '2'
-                 AND bridging_sep.no_sep > 2
-                 AND bridging_sep.no_sep = (
-                     SELECT MIN(bridging_sep_inner.no_sep)
-                     FROM bridging_sep bridging_sep_inner
-                     WHERE bridging_sep_inner.jnspelayanan = '2'
-                     AND bridging_sep_inner.no_rawat = bridging_sep.no_rawat
-                     AND bridging_sep_inner.no_sep > 2
-                 )
-                ) AS bridging_ralan
-            "), 'bridging_ralan.no_rawat', '=', 'reg_periksa.no_rawat')
-            ->where(function ($query) {
-                $query->orWhere('reg_periksa.no_rawat', '=', $this->cariNorawat)
-                    ->orWhere('reg_periksa.no_rkm_medis', '=', $this->cariNorawat)
-                    ->orWhere('bridging_ranap.no_sep', '=', $this->cariNorawat)
-                    ->orWhere('bridging_ralan.no_sep', '=', $this->cariNorawat);
-            })
-            ->whereIn('reg_periksa.status_lanjut', ['Ranap', 'Ralan'])
-            ->orderByRaw("CASE WHEN reg_periksa.status_lanjut = 'Ranap' THEN 1 ELSE 2 END") // Prioritaskan pasien rawat inap
-            ->orderBy('reg_periksa.tgl_registrasi', 'desc') // Urutkan berdasarkan tanggal registrasi terbaru
-            ->limit(10) // Batasi hasil maksimal 20 data
-            ->get();
+    {
+        if ($this->cariNorawat) {
+            $this->getPasien = DB::table('reg_periksa')
+                ->select(
+                    'reg_periksa.no_rawat',
+                    'reg_periksa.no_rkm_medis',
+                    'reg_periksa.tgl_registrasi',
+                    'pasien.nm_pasien',
+                    'pasien.jk',
+                    DB::raw("COALESCE(bridging_ranap.no_sep, bridging_ralan.no_sep, '-') AS no_sep"),
+                    DB::raw("
+                        CASE
+                            WHEN reg_periksa.status_lanjut = 'Ranap' THEN '1'
+                            WHEN reg_periksa.status_lanjut = 'Ralan' THEN '2'
+                            ELSE '-'
+                        END AS jnspelayanan
+                    "),
+                    'reg_periksa.status_lanjut'
+                )
+                ->join('pasien', 'reg_periksa.no_rkm_medis', '=', 'pasien.no_rkm_medis')
+                ->leftJoin('bridging_sep AS bridging_ranap', function ($join) {
+                    $join->on('bridging_ranap.no_rawat', '=', 'reg_periksa.no_rawat')
+                         ->where('bridging_ranap.jnspelayanan', '=', '1')
+                         ->where('bridging_ranap.no_sep', '>', 2);
+                })
+                ->leftJoin('bridging_sep AS bridging_ralan', function ($join) {
+                    $join->on('bridging_ralan.no_rawat', '=', 'reg_periksa.no_rawat')
+                         ->where('bridging_ralan.jnspelayanan', '=', '2')
+                         ->where('bridging_ralan.no_sep', '>', 2);
+                })
+                ->where(function ($query) {
+                    $query->orWhere('reg_periksa.no_rawat', '=', $this->cariNorawat)
+                        ->orWhere('reg_periksa.no_rkm_medis', '=', $this->cariNorawat)
+                        ->orWhere('bridging_ranap.no_sep', '=', $this->cariNorawat)
+                        ->orWhere('bridging_ralan.no_sep', '=', $this->cariNorawat);
+                })
+                ->whereIn('reg_periksa.status_lanjut', ['Ranap', 'Ralan'])
+                ->orderBy('reg_periksa.tgl_registrasi', 'desc')
+                ->get();
         } else {
             $this->getPasien = [];
         }
