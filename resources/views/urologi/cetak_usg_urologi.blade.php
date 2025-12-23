d9e6c2c7eac1d99d12f11476e451af58ec524c0d
IT RS BW <rsbwit@gmail.com>
2025-12-23 14:58:46 +0700

    Urologi1

--- resources/views/urologi/cetak_usg_urologi.blade.php
+++ resources/views/urologi/cetak_usg_urologi.blade.php
@@ -6,14 +6,15 @@
 
 <style>
 @page{
-    size: A4;
-    margin: 25mm;
+    size:A4;
+    margin:25mm;
 }
 
 body{
-    font-family: "Times New Roman", serif;
-    font-size: 13px;
-    color: #000;
+    font-family:"Times New Roman", serif;
+    font-size:14px;
+    color:#000;
+    /* font-weight:bold; */
 }
 
 /* =========================
@@ -24,33 +25,32 @@ body{
     display:flex;
     align-items:center;
     border-bottom:2px solid #000;
-    padding-bottom:10px;
-    margin-bottom:20px;
+    padding-bottom:8px;
+    margin-bottom:18px;
 }
 
 .kop .logo{
-    width:120px;
-    text-align:left;
+    width:110px;
 }
 
 .kop .logo img{
-    height:80px;
+    height:75px;
 }
 
 .kop .kop-text{
     flex:1;
     text-align:center;
+    padding-right:35px;
 }
 
 .kop .kop-text .nama{
-    font-size:20px;
+    font-size:18px;
     font-weight:bold;
     text-transform:uppercase;
 }
 
 .kop .kop-text .alamat{
-    font-size:13px;
-    margin-top:2px;
+    font-size:12px;
     line-height:1.4;
 }
 
@@ -59,13 +59,13 @@ body{
 ========================= */
 .judul{
     text-align:center;
-    font-size:15px;
+    font-size:14px;
     font-weight:bold;
-    margin:15px 0 20px 0;
+    margin:15px 0 18px;
 }
 
 /* =========================
-   IDENTITAS PASIEN
+   IDENTITAS
 ========================= */
 table.identitas{
     width:100%;
@@ -74,53 +74,63 @@ table.identitas{
 }
 
 table.identitas td{
-    padding:4px 6px;
+    padding:3px 5px;
     vertical-align:top;
 }
 
-table.identitas td.label{
-    width:15%;
-}
-
-table.identitas td.separator{
-    width:2%;
-}
+table.identitas td.label{width:15%;}
+table.identitas td.separator{width:2%;}
 
 /* =========================
-   HASIL USG
+   HASIL
 ========================= */
 .box{
     border:1px solid #000;
     padding:12px;
-    margin-bottom:18px;
+    margin-bottom:22px;
 }
 
 .hasil{
-    min-height:180px;
+    min-height:170px;
     line-height:1.6;
 }
 
 /* =========================
-   TTD + QR (QR DI ATAS NAMA)
+   TTD + QR (KANAN, QR DI ATAS NAMA)
 ========================= */
 .ttd-wrapper{
     width:100%;
-    margin-top:45px;
+    margin-top:20px;
+    display:flex;
+    justify-content:flex-end;
 }
 
-.ttd-kiri{
-    width:50%;
-    text-align:left;
+.ttd{
+    width:42%;
+    text-align:center;
 }
 
-.ttd-kiri img{
-    width:100px;
-    height:100px;
-    margin:10px 0;
+.ttd .tanggal{
+    margin-bottom:2px;
 }
 
-.ttd-kiri .nama{
+.ttd img{
+    width:95px;
+    height:95px;
+    margin:8px auto 6px;
+    display:block;
+}
+
+.ttd .nama{
     font-weight:bold;
+    text-decoration:underline;
+    margin-top:4px;
+    font-size:13px;
+}
+
+.ttd .jabatan{
+    font-size:11px;
+    margin-top:2px;
 }
 </style>
 
@@ -128,7 +138,7 @@ table.identitas td.separator{
 <body onload="window.print()">
 
 <!-- =========================
-     KOP SURAT
+     KOP
 ========================= -->
 <div class="kop">
     <div class="logo">
@@ -163,7 +173,6 @@ table.identitas td.separator{
     <td class="label">Poli</td><td class="separator">:</td>
     <td>{{ $data->nm_poli }}</td>
 </tr>
-
 <tr>
     <td class="label">Nama Pasien</td><td class="separator">:</td>
     <td>{{ $data->nm_pasien }}</td>
@@ -171,7 +180,6 @@ table.identitas td.separator{
     <td class="label">Tanggal</td><td class="separator">:</td>
     <td>{{ date('d-m-Y', strtotime($data->created_at)) }}</td>
 </tr>
-
 <tr>
     <td class="label">JK / Umur</td><td class="separator">:</td>
     <td>{{ $data->jk }} / {{ $data->umurdaftar }}</td>
@@ -179,7 +187,6 @@ table.identitas td.separator{
     <td class="label">Jam</td><td class="separator">:</td>
     <td>{{ date('H:i:s', strtotime($data->created_at)) }}</td>
 </tr>
-
 <tr>
     <td class="label">No. Rawat</td><td class="separator">:</td>
     <td colspan="4">{{ $data->no_rawat }}</td>
@@ -187,7 +194,7 @@ table.identitas td.separator{
 </table>
 
 <!-- =========================
-     HASIL USG
+     HASIL PEMERIKSAAN
 ========================= -->
 <div class="box hasil">
     <strong>Hasil Pemeriksaan :</strong><br><br>
@@ -195,20 +202,23 @@ table.identitas td.separator{
 </div>
 
 <!-- =========================
-     TTD + QR
+     TTD & QR
 ========================= -->
 <div class="ttd-wrapper">
-    <div class="ttd-kiri">
-        Bandar Lampung, {{ date('d-m-Y') }}<br>
-        Dokter Pemeriksa<br><br>
+    <div class="ttd">
+        <div class="tanggal">
+            Bandar Lampung, {{ date('d-m-Y') }}
+        </div>
+        Dokter Pemeriksa
 
         <!-- QR CODE -->
-        <img src="data:image/png;base64,{{ $qrBase64 }}"
-            style="margin-left:20px;">
+        <img src="data:image/png;base64,{{ $qrBase64 }}">
 
-        <!-- NAMA DOKTER -->
         <div class="nama">
             {{ $data->nm_dokter }}
+        </div>
+        <div class="jabatan">
+            Spesialis Urologi
         </div>
     </div>
 </div>
