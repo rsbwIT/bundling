6e0798cad2d0a56b9f102b6981288be68a8e8078
IT RS BW <rsbwit@gmail.com>
2026-04-21 08:31:40 +0700

    pasien lebih dari 1 kali berobat3

--- resources/views/layout/layoutDashboard.blade.php
+++ resources/views/layout/layoutDashboard.blade.php
@@ -1486,6 +1486,15 @@
                                     </a>
                                 </li>
                             </ul>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="{{ url('/pasien-lebih-dari-1') }}" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Pasien >1 Berobat</p>
+                                    </a>
+                                </li>
+                            </ul>
+                            
                         </li>
                         <li class="nav-item">
                             <a href="#" class="nav-link">
