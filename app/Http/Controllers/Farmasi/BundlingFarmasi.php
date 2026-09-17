3aaf817d6146cf6454c9178e6652526edbcd217e
IT RS BW <rsbwit@gmail.com>
2026-09-17 10:42:16 +0700

    BUNDLING FARMASI1

--- app/Http/Controllers/Farmasi/BundlingFarmasi.php
+++ app/Http/Controllers/Farmasi/BundlingFarmasi.php
@@ -264,7 +264,7 @@ class BundlingFarmasi extends Controller
         ]);
 
         $no_rawatSTR = str_replace('/', '', $noRawat);
-        $pdfFilename = 'SEP-RESEP-' . substr($noSep, -5) . '.pdf';
+        $pdfFilename = substr($noSep, -5) . '.pdf';
 
         Storage::disk('public')->put('file_sepresep_farmasi/' . $pdfFilename, $pdf->output());
 
